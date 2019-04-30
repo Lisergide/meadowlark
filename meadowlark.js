ba84f0f0776cc03594df31d9e33cae375ba80213
Лисичкин Борис Александрович <BALisichkin@alfabank.ru>
2019-04-30 14:56:41 +0300

    create project meadowlark

---
+++ meadowlark.js
@@ -0,0 +1,51 @@
+const express = require('express');
+
+const app = express();
+
+// Установка механизма представления handlebars
+const handlebars = require('express-handlebars')
+    .create({
+        defaultLayout: 'main'
+    });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+const fortunes = [
+    'Победи свои страхи, или они победят тебя.',
+    'Рекам нужны истоки',
+    'Не бойся неведомого',
+    'Тебя ждет приятный сюрприз',
+    'Будь проще везде, где только можно.'
+];
+
+app.set('port', process.env.PORT || 3021);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/about', function (req, res) {
+    const randomFortune =
+    fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', { fortune: randomFortune });
+});
+
+// Обобщенный обработчик 404 (промежуточное ПО)
+app.use(function (req, res) {
+    res.status(404);
+    res.render('404');
+});
+
+// Обработчик ошибки 500 (промежуточное ПО)
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Express запущен на http://localhost:' +
+        app.get('port') + ': нажмите Ctrl+C для завершения.');
+});
